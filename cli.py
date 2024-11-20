404fd54cccf6a4b1b6a3bb2198f2300a8102a278
jejehus <jeremie.huser@epfl.ch>
2024-11-20 19:41:52 +0100

    Revert "tests"
    This reverts commit 745c2b35846c1764f15e636c8181ca787ca4deb9.

--- cli.py
+++ cli.py
@@ -3,7 +3,6 @@ from main import *
 import time
 import threading
 import serial
-import math
 
 SERIAL_PORT = 'COM3'
 BAUD_RATE = 500000
@@ -16,8 +15,9 @@ ser = serial.Serial(SERIAL_PORT, BAUD_RATE)
 
 # Global flag to stop threads
 stop_threads = False
-target_positions = []
+target_positions = [0]
 # Function to get current position and write to CSV
+
 
 def get_current_position_loop(motors_current, csv_file):
     global target_positions
@@ -26,25 +26,58 @@ def get_current_position_loop(motors_current, csv_file):
         writer.writerow(['Timestamp', 'Actual Position', 'Target Position', 'IMU Data'])
         while not stop_threads:
             positions = []
+            position_threads = []
             for motor in motors_current:
-                position = get_current_position(motor[0], motor[1], motor[2], motor[3])
-                positions.append(position)
+                position_thread = threading.Thread(target=lambda p, m=motor: p.append(get_current_position(m[0], m[1], m[2], m[3])), args=(positions,))
+                position_threads.append(position_thread)
+
+            # Start all position threads
+            for position_thread in position_threads:
+                position_thread.start()
+
+            # Join all position threads
+            for position_thread in position_threads:
+                position_thread.join()
+
             line = 0
             if ser.in_waiting > 0:
-                line = ser.readline()
-            writer.writerow([time.time(), positions, target_positions, line])
+                line = ser.readline().decode('utf-8').strip()
+            writer.writerow([time.time(), str(positions[0]) + ";" + str(positions[1]), target_positions[0], line])
             file.flush()
 
 # Function to move to position in a loop
 def go_to_position_loop(motors_current, positions, cycles):
     global target_positions
     while cycles > 0:
+        motor_threads = []
         for motor in motors_current:
-            move_to_position(motor[0], motor[1], motor[2], motor[3], positions[0])
+            motor_thread = threading.Thread(target=move_to_position, args=(motor[0], motor[1], motor[2], motor[3], positions[0]))
+            motor_threads.append(motor_thread)
+
+        # Start all motor threads
+        for motor_thread in motor_threads:
+            motor_thread.start()
+
+        # Join all motor threads
+        for motor_thread in motor_threads:
+            motor_thread.join()
+
         target_positions = [positions[0]]
         time.sleep(0.5)
+
+        motor_threads = []
         for motor in motors_current:
-            move_to_position(motor[0], motor[1], motor[2], motor[3], positions[1])
+            motor_thread = threading.Thread(target=move_to_position, args=(motor[0], motor[1], motor[2], motor[3], positions[1]))
+            motor_threads.append(motor_thread)
+
+        # Start all motor threads
+        for motor_thread in motor_threads:
+            motor_thread.start()
+
+        # Join all motor threads
+        for motor_thread in motor_threads:
+            motor_thread.join()
+
         target_positions = [positions[1]]
         time.sleep(0.5)
         cycles -= 1
@@ -112,7 +145,7 @@ def move():
     num_of_movements = int(input('Please enter the amount of movements you would like to make: '))
 
     positions = [max_position, min_position]
-    csv_file = 'C:/Users/Jérémie Huser/Documents/Epfl documents/Ma3/PDS_Gimbal/Control/2024_I_AV_SW_MAXON/' + 'positions_' + str(time.time()) + '.csv'
+    csv_file = 'positions_' + str(time.time()) + '.csv'
 
     thread_motor = threading.Thread(target=go_to_position_loop, args=(motors, positions, num_of_movements))
     thread_pos = threading.Thread(target=get_current_position_loop, args=(motors, csv_file))
@@ -123,84 +156,6 @@ def move():
     thread_motor.join()
     stop_threads = True
     thread_pos.join()
-
-# def circle():
-#     global stop_threads
-#     stop_threads = False
-#     print('Please enter the radius of the circle:')
-#     radius = int(input('Radius: '))
-#     angular_speed = float(input('Please enter the angular speed (in radians per second): '))
-#     num_of_movements = int(input('Please enter the number of circles you would like to make: '))
-#     num_points = int(input('Please enter the number of points to define the circle (higher = smoother): '))
-
-#     # Ensure the radius and angular speed are within limits
-#     if radius <= 0 or angular_speed <= 0:
-#         print('Invalid radius or angular speed. Please enter positive values.')
-#         return
-
-#     period = 2 * math.pi / angular_speed  # Time to complete one circle
-#     time_step = period / num_points       # Time between each calculated velocity change
-#     total_points = num_points * num_of_movements
-
-#     # Circular velocity control
-#     for i in range(total_points):
-#         angle = (2 * math.pi * i / num_points) % (2 * math.pi)
-#         # velocity_x = -radius * angular_speed * math.sin(angle)
-#         # velocity_y = radius * angular_speed * math.cos(angle)
-#         velocity_x = int(-radius * angular_speed * math.sin(angle)) 
-#         velocity_y = int(radius * angular_speed * math.cos(angle))  
-
-#         # Send calculated velocities to the motors
-#         move_with_velocity(motors[0][0], motors[0][1], motors[0][2], motors[0][3], velocity_x)
-#         move_with_velocity(motors[1][0], motors[1][1], motors[1][2], motors[1][3], velocity_y)
-
-#         # Allow the motors to hold this velocity for the time step
-#         # time.sleep(time_step)
-
-#     # Stop the motors after completing the motion
-#     move_with_velocity(motors[0][0], motors[0][1], motors[0][2], motors[0][3], 0)
-#     move_with_velocity(motors[1][0], motors[1][1], motors[1][2], motors[1][3], 0)
-
-#     print('Circular motion complete.')
-
-def circle():
-    global stop_threads
-    stop_threads = False
-    print('Please enter the radius of the circle:')
-    radius = int(input('Radius: '))
-    num_of_movements = int(input('Please enter the number of circles you would like to make: '))
-    num_points = int(input('Please enter the number of points to define the circle (higher = smoother): '))
-
-    # Ensure the radius is within limits
-    if radius <= 0:
-        print('Invalid radius. Please enter a positive value.')
-        return
-
-    # Define the positions for the circle
-    center_x, center_y = 0, 0  # Assuming the center is at (0, 0)
-    circle_points = [
-        (
-            int(center_x + radius * math.cos(2 * math.pi * i / num_points)),
-            int(center_y + radius * math.sin(2 * math.pi * i / num_points))
-        )
-        for i in range(num_points)
-    ]
-
-    # Perform the circular motion for the given number of movements
-    for _ in range(num_of_movements):
-        for point in circle_points:
-            x_pos, y_pos = point
-            # Send motor commands to reach the calculated position
-            move_to_position(motors[0][0], motors[0][1], motors[0][2], motors[0][3], x_pos)
-            move_to_position(motors[1][0], motors[1][1], motors[1][2], motors[1][3], y_pos)
-            # Allow some time for the motors to move to each point
-            time.sleep(0.0001)
-
-    # Return the gimbal to the center position after completing the circles
-    move_to_position(motors[0][0], motors[0][1], motors[0][2], motors[0][3], center_x)
-    move_to_position(motors[1][0], motors[1][1], motors[1][2], motors[1][3], center_y)
-    print('Circle motion complete.')
-
 
 def intro():
     message_home = """
@@ -243,7 +198,6 @@ def main():
         print('• Move Motors (5)')
         print('• Get position (6)')
         print('• Terminate program (7)')
-        print('• Circle (8)')
         choice = int(input())
         if choice == 1:
             enable()
@@ -259,8 +213,6 @@ def main():
             get_current_position_motors()
         elif choice == 7:
             exit()
-        elif choice == 8:
-            circle()
         else:
             print('Invalid input, please enter a valid number')
             main()
