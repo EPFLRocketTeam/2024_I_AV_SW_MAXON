336b011f18c9cdae60465be5a8ddd1b2c512211d
ERT RPI <ERTRPI@example.com>
2025-03-23 09:42:59 +0100

    modified for RPI

--- tf_valves/move_motors.py
+++ tf_valves/move_motors.py
@@ -4,16 +4,16 @@ import RPi.GPIO as GPIO
 
 # CONFIG VARIABLES
 NODE_ID = 1  # Node ID
-USB = b'USB1'   # USB port
-VALVE_PIN = 16  # GPIO pin
-VALVE_OPEN_INCREMENT = 212992   # 90 degrees
+USB = b'USB0'   # USB port
+VALVE_PIN = 5  # GPIO pin
+VALVE_OPEN_INCREMENT = 30000   # 90 degrees 212992
 VELOCITY = 6000   # RPM
 ACCELERATION = 80000    # RPM/s
 DECELERATION = 80000    # RPM/s
 
 # setup GPIO Pins
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(VALVE_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)   # VALVE control pin
+GPIO.setup(VALVE_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)   # VALVE control pin
 
 epos, keyhandle, NodeID, pErrorCode, pDeviceErrorCode = epos_setup(NODE_ID, USB, VELOCITY, ACCELERATION, DECELERATION)
 
