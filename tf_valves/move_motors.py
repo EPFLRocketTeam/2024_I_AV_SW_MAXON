8b3370e83b7edf9c78ae161f03a370db28907511
yann <yann@gaspoz-fleiner.com>
2025-03-23 09:55:31 +0100

    added 2 motor support

--- tf_valves/move_motors.py
+++ tf_valves/move_motors.py
@@ -3,24 +3,31 @@ from main import *
 import RPi.GPIO as GPIO
 
 # CONFIG VARIABLES
-NODE_ID = 1  # Node ID
-USB = b'USB0'   # USB port
-VALVE_PIN = 5  # GPIO pin
-VALVE_OPEN_INCREMENT = 30000   # 90 degrees 212992
+NODE_ID_1 = 1  # Node ID
+NODE_ID_2 = 2
+USB_1 = b'USB0'   # USB port
+USB_2 = b'USB1'
+VALVE_PIN_1 = 5  # GPIO pin
+VALVE_PIN_2 = 6
+VALVE_OPEN_INCREMENT = 55000   # 90 degrees 212992
 VELOCITY = 6000   # RPM
 ACCELERATION = 80000    # RPM/s
 DECELERATION = 80000    # RPM/s
 
 # setup GPIO Pins
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(VALVE_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)   # VALVE control pin
+GPIO.setup(VALVE_PIN_1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)   # VALVE_1 control pin
+GPIO.setup(VALVE_PIN_2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)   # VALVE_2 control pin
 
-epos, keyhandle, NodeID, pErrorCode, pDeviceErrorCode = epos_setup(NODE_ID, USB, VELOCITY, ACCELERATION, DECELERATION)
+epos_1, keyhandle_1, NodeID_1, pErrorCode_1, pDeviceErrorCode_1 = epos_setup(NODE_ID_1, USB_1, VELOCITY, ACCELERATION, DECELERATION)
+epos_2, keyhandle_2, NodeID_2, pErrorCode_2, pDeviceErrorCode_2 = epos_setup(NODE_ID_2, USB_2, VELOCITY, ACCELERATION, DECELERATION)
 
 while True:
-    if GPIO.input(VALVE_PIN) == GPIO.LOW:
-        print('Valve closed')
-        move_to_position(epos, keyhandle, NodeID, pErrorCode, 0)
+    if GPIO.input(VALVE_PIN_1) == GPIO.LOW:
+        move_to_position(epos_1, keyhandle_1, NodeID_1, pErrorCode_1, 0)
     else:
-        print('Valve open')
-        move_to_position(epos, keyhandle, NodeID, pErrorCode, VALVE_OPEN_INCREMENT)
+        move_to_position(epos_1, keyhandle_1, NodeID_1, pErrorCode_1, VALVE_OPEN_INCREMENT)
+    if GPIO.input(VALVE_PIN_2) == GPIO.LOW:
+        move_to_position(epos_2, keyhandle_2, NodeID_2, pErrorCode_2, 0)
+    else:
+        move_to_position(epos_2, keyhandle_2, NodeID_2, pErrorCode_2, VALVE_OPEN_INCREMENT)
