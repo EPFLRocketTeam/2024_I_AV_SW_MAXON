a6fb25ac02bfc48beedd6bc8d5339f6323d5c566
yann <yann@gaspoz-fleiner.com>
2025-03-27 14:28:15 +0100

    bug

--- tf_valves/move_motors.py
+++ tf_valves/move_motors.py
@@ -58,5 +58,3 @@ while True:
             go_to_position(epos_2, keyhandle_2, NodeID_2, pErrorCode_2, HOMING_INCREMENT)
         time.sleep(1)
         set_home_position(epos_2, keyhandle_2, NodeID_2, pErrorCode_2)
-    time.sleep(0.5)
-
