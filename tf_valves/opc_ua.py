641fc406f76187b8f94b991d7e2d210ce896a84d
Tim Vial <53138133+TimVial@users.noreply.github.com>
2025-04-06 11:19:54 +0200

    opcua

--- tf_valves/opc_ua.py
+++ tf_valves/opc_ua.py
@@ -2,10 +2,10 @@ from opcua import Client
 import time
 
 # Adresse du serveur OPC UA sur l’automate Wago (remplacez l’IP par celle de votre automate)
-OPCUA_SERVER_URL = "opc.tcp://192.168.0.10:4840"
+OPCUA_SERVER_URL = "opc.tcp://192.168.1.17:4840"
 
 # Noeud de la variable à lire (ce NodeId est un exemple, ajustez-le selon votre projet)
-NODE_ID = "ns=2;s=TempConsigne"  # ns=2 : namespace, s=TempConsigne : nom de la variable
+NODE_ID = "ns=5;s=b_Homing_E"  # ns=2 : namespace, s=TempConsigne : nom de la variable
 
 # Connexion au serveur OPC UA
 client = Client(OPCUA_SERVER_URL)
@@ -15,8 +15,8 @@ try:
 
     while True:
         # Lire la valeur de la variable
-        temp_value = client.get_node(NODE_ID).get_value()
-        print(f"Température consigne : {temp_value:.2f} °C")
+        b_Homing_E = client.get_node(NODE_ID).get_value()
+        print(f"b_Homing_E : {b_Homing_E:.2f}" )
 
         # Pause de 100 ms avant la prochaine lecture
         time.sleep(0.1)
